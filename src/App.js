5b10e4729bebba97a9bec2ae57656e6f1fa63531
Bahlul19 <bahlul.tausif@gmail.com>
2021-07-02 18:37:36 +0600

    Added the top banner section with component and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import TopBanner from "./components/TopBanner/TopBanner";
 
 function App() {
   return (
     <div>
+
+      <TopBanner />
+      
     </div>
+
   );
 }
 
