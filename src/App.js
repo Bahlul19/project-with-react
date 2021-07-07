a5e6c97236cdb602d0ac96990d16690fd85a5dbc
Bahlul19 <bahlul.tausif@gmail.com>
2021-07-07 19:13:12 +0600

    Added navigation functionality for scrolling

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import TopBanner from "./components/TopBanner/TopBanner";
 
 import Services from './components/Services/Services';
 
+import TopNavigation from './components/TopNavigation/TopNavigation';
+
 function App() {
   return (
     <div>
-
+      <TopNavigation />
       <TopBanner />
       <Services />
       
---
+++ src/components/TopNavigation/TopNavigation.js
@@ -0,0 +1,61 @@
+import React, {Component,Fragment} from 'react';
+import {Nav, Navbar, NavDropdown} from "react-bootstrap";
+import whiteLogo from '../../asset/image/navlogo.svg';
+import blueLogo from '../../asset/image/navlogoScroll.svg';
+
+class TopNavigation extends Component {
+    
+    
+    constructor(){
+        super();
+
+        this.state = {
+            //navbarTitle is a property
+            navbarTitle : "navTitle",
+            navbarLogo : [whiteLogo],
+            navbarBackground : "navBackground"
+        }
+    }
+
+    onScroll = () => {
+        if(window.scrollY > 100)
+        {
+            this.setState({navbarTitle : 'navTitleScroll', navbarLogo:[blueLogo], navbarBackground:'navBackgroundScroll'})
+        }
+        else if(window.scrollY < 100)
+        {
+            this.setState({navbarTitle: 'navTitle', navbarLogo:[whiteLogo], navbarBackground:'navBackground'})
+        }
+    }
+
+    componentDidMount(){
+        window.addEventListener('scroll', this.onScroll)
+    }
+
+
+    render() {
+        return (
+            <Fragment>
+                <Navbar className={this.state.navbarBackground} fixed="top" collapseOnSelect expand="lg" variant="dark">
+                    <Navbar.Brand href="#home" className={this.state.navbarTitle}><img src={this.state.navbarLogo} /> Bahlul Siddiquee</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto">
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">HOME</Nav.Link>
+                            <Nav.Link href="#deets">SERVICES</Nav.Link>
+                            <Nav.Link href="#deets">COURSES</Nav.Link>
+                            <Nav.Link href="#deets">PORTFOLIO</Nav.Link>
+                            <Nav.Link href="#deets">CONTACT</Nav.Link>
+                            <Nav.Link href="#deets">ABOUT</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                
+            </Fragment>
+        );
+    }
+}
+
+export default TopNavigation;
