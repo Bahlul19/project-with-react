191093866791fb7780c2367c1bb43180381e6504
Bahlul19 <bahlul.tausif@gmail.com>
2021-07-07 15:34:07 +0600

    Added the services component into the homepage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import TopBanner from "./components/TopBanner/TopBanner";
 
+import Services from './components/Services/Services';
+
 function App() {
   return (
     <div>
 
       <TopBanner />
+      <Services />
       
     </div>
 
