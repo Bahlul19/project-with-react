bed3f80476cd8592f4a39356339fae4b5ae22c5c
Bahlul19 <bahlul.tausif@gmail.com>
2021-08-03 13:23:30 +0600

    Done with page top section for all the page and call it from the app.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import TopNavigation from './components/TopNavigation/TopNavigation.jsx';
+import PageTop from './components/PageTop/PageTop.jsx';
 
-import HomePage from "./pages/HomePage.jsx";
+// import HomePage from "./pages/HomePage.jsx";
 
 function App() {
   return (
     <div>
 
-      <HomePage />
+      <TopNavigation />
+      <PageTop pagetitle ="About Me"/>
+      {/* sending a props to the specific page */}
       
     </div>
 
