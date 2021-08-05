a9fbdf41dffc6c520b73a02ed84861aff2b46560
Bahlul19 <bahlul.tausif@gmail.com>
2021-08-05 14:16:31 +0600

    facing issue with service contact page, bootstrap is not working

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 // import TopNavigation from './components/TopNavigation/TopNavigation.jsx';
 // import PageTop from './components/PageTop/PageTop.jsx';
 // import AboutDescription from './components/AboutDescription/AboutDescription.jsx';
+import ContactSection from './components/ContactSection/ContactSection.jsx';
 
 // import AboutPage from './pages/AboutPage';
-import PortfolioPage from './pages/PortfolioPage';
+// import ContactSection from './pages/ContactSection';
 
 // import HomePage from "./pages/HomePage.jsx";
 
@@ -12,7 +13,7 @@ function App() {
   return (
     <div>
 
-     <PortfolioPage />
+    <ContactSection />
       
     </div>
 
