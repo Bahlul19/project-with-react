657fe61a85d78ba5ff1b81c8dd243026e9371289
Bahlul19 <bahlul.tausif@gmail.com>
2021-08-05 14:17:19 +0600

    facing issue with service contact page, bootstrap is not working

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 // import TopNavigation from './components/TopNavigation/TopNavigation.jsx';
 // import PageTop from './components/PageTop/PageTop.jsx';
 // import AboutDescription from './components/AboutDescription/AboutDescription.jsx';
-import ContactSection from './components/ContactSection/ContactSection.jsx';
+ import ContactSection from './components/ContactSection/ContactSection.jsx';
 
 // import AboutPage from './pages/AboutPage';
-// import ContactSection from './pages/ContactSection';
 
-// import HomePage from "./pages/HomePage.jsx";
+//import HomePage from "./pages/HomePage.jsx";
 
 function App() {
   return (
--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -1,5 +1,5 @@
 import React, {Component,Fragment} from 'react';
-import {Button, Col, Container, Form, Row} from "react-bootstrap";
+import {Col, Container, Row} from 'react-bootstrap';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEnvelope, faPhone} from "@fortawesome/free-solid-svg-icons";
 
@@ -7,44 +7,26 @@ class ContactSection extends Component {
     render() {
         return (
             <Fragment>
-                <Container className="mt-5">
+
+               <Container>
+
                     <Row>
-                        <Col lg={6} md={6} sm={12}>
-                            <h1 className="serviceName">Quick Connect</h1>
-
-                            <Form>
-                                <Form.Group>
-                                    <Form.Label className="serviceDescription" >Name</Form.Label>
-                                    <Form.Control type="text" />
-                                </Form.Group>
-
-                                <Form.Group>
-                                    <Form.Label className="serviceDescription" >Email Address</Form.Label>
-                                    <Form.Control type="email" />
-                                </Form.Group>
-
-                                <Form.Group>
-                                    <Form.Label className="serviceDescription" >Message</Form.Label>
-                                    <Form.Control  as="textarea" rows="3" />
-                                </Form.Group>
-
-
-                                <Button variant="primary" type="submit">
-                                    Submit
-                                </Button>
-                            </Form>
-
+                        <Col lg={3} md={3} sm={12}>
+                        <p>Text</p>
                         </Col>
-
-
-                        <Col lg={6} md={6} sm={12}>
-                            <h1 className="serviceName">Discuss Now</h1>
-                            <p className="serviceDescription" >#79/6 Padma Residential Aria, 3rd Floor Front Side, Rajshahi</p>
-                            <p className="serviceDescription" > <FontAwesomeIcon  icon={faEnvelope} /> Engr.Rabbil@yahoo.com</p>
-                            <p className="serviceDescription" > <FontAwesomeIcon  icon={faPhone} /> +8801701063280</p>
+                        <Col lg={3} md={3} sm={12}>
+                        <p>Text</p>
+                        </Col>
+                        <Col lg={3} md={3} sm={12}>
+                        <p>Text</p>
+                        </Col>
+                        <Col lg={3} md={3} sm={12}>
+                        <p>Text</p>
                         </Col>
                     </Row>
-                </Container>
+
+               </Container>
+
             </Fragment>
         );
     }
