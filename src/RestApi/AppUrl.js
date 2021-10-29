a901e35e6afc0e6950faee29d7579f412f621498
Bahlul19 <bahlul.siddiquee@sjinnovation.com>
2021-10-29 10:26:17 +0600

    Added the axios code for fetching the data from the API

---
+++ src/RestApi/AppUrl.js
@@ -0,0 +1,24 @@
+class AppUrl {
+
+    static BaseUrl='http://127.0.0.1:8000';
+
+    static HomeTopTitle=this.BaseURL+'/home-top-title';
+    static Technology=this.BaseURL+'/technology';
+    static Total_project_and_client=this.BaseURL+'/total-project-and-client';
+    static Video_home=this.BaseURL+'/video-home';
+    static Projectdetail=this.BaseURL+'/projectdetail';
+    static Allprojects=this.BaseURL+'/allprojects';
+    static Projecthome=this.BaseURL+'/projecthome';
+    static Services=this.BaseURL+'/services';
+    static Information=this.BaseURL+'/information';
+    static Footer=this.BaseURL+'/footer';
+    static Coursedetails=this.BaseURL+'/coursedetails';
+    static Allcourses=this.BaseURL+'/allcourses';
+    static Coursehome=this.BaseURL+'/coursehome';
+    static Sendcontact=this.BaseURL+'/sendcontact';
+    static Clientreview=this.BaseURL+'/clientreview';
+    static Chartdata=this.BaseURL+'/chartdata';
+
+}
+
+export default AppUrl;
